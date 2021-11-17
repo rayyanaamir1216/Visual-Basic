429f106231494987eb3eb6c1cb26457de6156de7
Rayyan Alvi <94387680+rayyanaamir1216@users.noreply.github.com>
2021-11-17 10:30:27 +0500

    Add files via upload

---
+++ ASSIGNMENT 2 VISUAL BASIC.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1 As String
+        Dim Str2 As String
+        Dim charRev As String
+        Dim charRel, Char1 As String
+        Dim Counter As Integer
+
+        Str1 = ""
+        Str2 = ""
+        charRev = ""
+        charRel = ""
+        Char1 = ""
+        Counter = 0
+
+        Console.Write("Enter String: ")
+        Str1 = Console.ReadLine
+
+        Console.Write("Enter character to remove: ")
+        charRev = Console.ReadLine
+
+        Console.Write("Enter character to replce: ")
+        charRel = Console.ReadLine
+
+        For Counter = 1 To Len(Str1)
+            Char1 = Mid(Str1, Counter, 1)
+            If Char1 = charRev Then
+                Str2 = Str2 + charRel
+            Else
+                Str2 = Str2 + Char1
+            End If
+        Next
+
+        Console.Write("the new string is: " + Str2)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
