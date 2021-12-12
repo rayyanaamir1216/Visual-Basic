5fd9ff493912ae2cf896d3507656419b9a526217
Rayyan Alvi <94387680+rayyanaamir1216@users.noreply.github.com>
2021-12-12 23:40:36 +0500

    Add files via upload

---
+++ Assignment 6(Visual Basic)DONUTCOUNT.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        'DECLERATION
+        Dim Int As Integer
+        Dim Donuts As Boolean
+
+        'INITIALISATION
+        Int = 0
+        Donuts = True
+
+        'PROCEDURE
+
+        Console.Write("Input Number: ")
+        Int = Console.ReadLine
+
+        If Int > 1 And Int < 10 Then
+            Donuts = True
+        End If
+
+        If Int > 9 Then
+            Donuts = False
+        End If
+
+        'OUTPUT
+
+        If Donuts = True Then
+            Console.WriteLine("Number of Donuts: " & Int)
+            Console.ReadKey()
+        Else
+            Console.WriteLine("Number of Donuts: Many")
+            Console.ReadKey()
+        End If
+    End Sub
+
+End Module
